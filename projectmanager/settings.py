e02baa4cc2cf866cd93fccb789cf78b4207dcc6e
Posiislight <adelekeolamiposi@gmail.com>
2025-04-08 07:07:33 +0100

    fixed allow hosts

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-j#cvv+c^x1%q-q*y)16ea4b)4y-zj*$&gww#tx%-os)gc=ul4@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','invigorating-playfulness-projectmanagementprod.up.railway.app']
 
 
 # Application definition
