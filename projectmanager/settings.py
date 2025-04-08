0dff85b6cfab67a960ead6ebf954bca477a755ac
Posiislight <adelekeolamiposi@gmail.com>
2025-04-08 07:14:51 +0100

    trying to fix csrf error

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -140,3 +140,6 @@ NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://invigorating-playfulness-projectmanagementprod.up.railway.app',
+]
