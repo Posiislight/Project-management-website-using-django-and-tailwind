c4b13a41c5c34e986e4dc25aa481f1f1e75d53d9
Posiislight <adelekeolamiposi@gmail.com>
2025-06-24 13:14:07 +0100

    added render allowed host

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-j#cvv+c^x1%q-q*y)16ea4b)4y-zj*$&gww#tx%-os)gc=ul4@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','invigorating-playfulness-projectmanagementprod.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','project-management-website-using-django.onrender.com']
 
 
 # Application definition
